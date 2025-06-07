dc4201aac64b94d3f4e11bfc3ff84c95ea60d07e
ljbarre3 <ljbarre3@asu.edu>
2025-06-07 00:43:03 -0700

    Animation Concept for final Score for Ihop Reviews

---
+++ IhoppingArizona-Frontend/src/components/FinalPancakeStack.tsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+// @ts-ignore
+import PancakeIcon from '../assets/Pancake-Stack.svg?react';
+
+type FinalPancakeStackProps = {
+    finalScore: number; // e.g. 37 out of 43
+    maxScore?: number;
+};
+
+export function FinalPancakeStack({ finalScore, maxScore = 43 }: FinalPancakeStackProps) {
+    const pancakes = Math.round((finalScore / maxScore) * 10);
+    return (
+        <div style={{ display: "flex", flexDirection: "column-reverse", alignItems: "center" }}>
+            {Array.from({ length: pancakes }).map((_, i) => (
+                <motion.div
+                    key={i}
+                    initial={{ opacity: 0, y: -40 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: i * .5 }}
+                    style={ {marginTop: -30}}
+                >
+                    <PancakeIcon width={40} height={40} />
+                </motion.div>
+            ))}
+        </div>
+    );
+}
