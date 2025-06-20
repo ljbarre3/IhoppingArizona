590d8588ff2959042fae7a196c7ecb83656ad782
ljbarre3 <ljbarre3@asu.edu>
2025-06-19 19:15:52 -0700

    Changed CORS policy for new frontend domain.

--- IhoppingArizona-Backend/src/main/java/org/example/ihoparizona/Config/SecurityConfig.java
+++ IhoppingArizona-Backend/src/main/java/org/example/ihoparizona/Config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://ihoppingarizonafrontend.onrender.com"));
+        config.setAllowedOrigins(List.of("https://ihoppingarizonafrontend.onrender.com", "https://www.ihoppingarizona.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
